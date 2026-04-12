3ad0e2a47a7ea2fffaf56a9ade3f22f8a3b0ddaa
MoIsa77 <modesign111@gmail.com>
2026-04-12 22:42:43 +0700

    1

--- app/page.js
+++ app/page.js
@@ -758,7 +758,7 @@ export default function Home() {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     className="w-full border border-slate-200 bg-slate-50 px-4 py-2.5 rounded-xl outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-400 font-medium text-slate-700 transition"
-                    placeholder="e.g. Johnson Panggabean"
+                    placeholder="e.g. Intan Kurnia Darsono"
                     required
                   />
                 </div>
